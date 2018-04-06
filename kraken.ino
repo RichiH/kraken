7befa7a05571365940cfdfd2da1d3f2634677306
Richard Hartmann <richih@richih.org>
2018-04-06 20:44:47 +0200

    Implement dew point and absolute humidity

--- kraken.ino
+++ kraken.ino
@@ -8,6 +8,8 @@
 
 #include <Wire.h>
 
+#include <math.h>
+
 
 #define LED D4
 #define SENSORPIN A0
@@ -25,6 +27,14 @@ float airPres, airTemperature, airHumidity, ppm, ppmbalanced, rzero;
 MQ135 gasSensor = MQ135(SENSORPIN);
 BME280I2C bme;
 
+// dew point, etc
+// see https://www.wetterochs.de/wetter/feuchte.html
+float saturation_vapor_pressure_hPa, vapor_pressure_hPa, dew_point_celsius, absolute_humidity_g_per_m3;
+const float a = 7.5;
+const float b = 237.3;
+const float R = 8314.3;
+const float mw = 18.016;
+float v;
 
 void handleRoot() {
 //  digitalWrite(led, 1);
@@ -52,13 +62,15 @@ void handleNotFound(){
 }
 
 String promResponse() {
-  String promPage ="kraken_temperature_celsius{sensor=\"BME280\"} " + String(airTemperature) + "\n" ;
-  promPage += "kraken_airpressure_hectopascals{sensor=\"BME280\"} " + String(airPres) + "\n";
-  promPage += "kraken_relative_humidity_percent{sensor=\"BME280\"} " + String(airHumidity) + "\n";
-  promPage += "kraken_rzero{sensor=\"MQ135\"} " + String(rzero) + "\n";
-  promPage += "kraken_raw{sensor=\"MQ135\"} " + String(sensorValue) + "\n";
-  promPage += "kraken_voc_ppm{sensor=\"MQ135\"} " + String(ppm) + "\n";
-  promPage += "kraken_voc_ppm_balanced{sensor=\"MQ135\"} " + String(ppmbalanced) + "\n";
+  String promPage ="kraken_temperature_celsius{sensor_type=\"BME280\"} " + String(airTemperature) + "\n" ;
+  promPage += "kraken_airpressure_hectopascal{sensor_type=\"BME280\"} " + String(airPres) + "\n";
+  promPage += "kraken_relative_humidity_percent{sensor_type=\"BME280\"} " + String(airHumidity) + "\n";
+  promPage += "kraken_dew_point_celsius " + String(dew_point_celsius) + "\n";
+  promPage += "kraken_absolute_humidity_g_per_m3 " + String(absolute_humidity_g_per_m3) + "\n";
+  promPage += "kraken_rzero{sensor_type=\"MQ135\"} " + String(rzero) + "\n";
+  promPage += "kraken_raw{sensor_type=\"MQ135\"} " + String(sensorValue) + "\n";
+  promPage += "kraken_voc_ppm{sensor_type=\"MQ135\"} " + String(ppm) + "\n";
+  promPage += "kraken_voc_ppm_balanced{sensor_type=\"MQ135\"} " + String(ppmbalanced) + "\n";
   return promPage;
 }
 
@@ -91,6 +103,7 @@ void setup(void){
     Serial.println("Could not find BME280 sensor!");
     delay(1000);
   }
+  Serial.println("Found BME280 sensor!");
 
   server.on("/", handleRoot);
 
@@ -106,6 +119,19 @@ void setup(void){
     rzero = gasSensor.getRZero();
     ppm = gasSensor.getPPM();
     ppmbalanced = gasSensor.getCorrectedPPM(airTemperature, airHumidity);
+
+//float a, b, R, mw, saturation_vapor_pressure_hPa, vapor_pressure_hPa, dew_point_celsius, absolute_humidity_g_per_m3;
+
+    // dew point, as per https://www.wetterochs.de/wetter/feuchte.html
+    saturation_vapor_pressure_hPa = 6.1078 * powf(10, ((a * airTemperature) / (b + airTemperature)));
+    vapor_pressure_hPa = (airHumidity / 100) * saturation_vapor_pressure_hPa;
+    v = log10(vapor_pressure_hPa/6.1078);
+    dew_point_celsius = b * v / (a - v);
+//    Serial.println("dew point" + String(dew_point_celsius) + "\n");
+
+    // absolute humidity
+    absolute_humidity_g_per_m3 = 100000 * mw / R  * vapor_pressure_hPa / (airTemperature + 273.15);
+//    Serial.println("abs humidity" + String(absolute_humidity_g_per_m3) + "\n");
 
     server.send(200, "text/plain", promResponse());
   });
