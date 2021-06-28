61c81cd0ab54e92e13435793df71c30986e039ee
Richard Hartmann <richih@richih.org>
2021-06-28 22:46:49 +0200

    Works
    Signed-off-by: Richard Hartmann <richih@richih.org>

--- bme_680/bme_680.ino
+++ bme_680/bme_680.ino
@@ -2,6 +2,7 @@
 #include "certificates.h"
 #include <PromLokiTransport.h>
 #include <PrometheusArduino.h>
+
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
@@ -9,46 +10,45 @@
 
 #define BME_MISO 12
 #define BME_MOSI 11
-#define BME_CS 10*/
+#define BME_CS 10
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_BME680 bme; // I2C
 
 
+
 PromLokiTransport transport;
 PromClient client(transport);
 
-// Create a write request for 2 series.
-WriteRequest req(2, 1024);
+//for dew point
+float saturationVaporPressurehPa, vaporPressurehPa, dewPointCelsius, absolute_humidity_g_per_m3;
+const float a = 7.5;
+const float b = 237.3;
+const float R = 8314.3;
+const float mw = 18.016;
+float v;
 
+// Create a write request for 2 series.
+WriteRequest req(10, 2048);
 // Check out https://prometheus.io/docs/practices/naming/ for metric naming and label conventions.
 // This library does not currently create different metric types like gauges, counters, and histograms
 // however internally Prometheus does not differentiate between these types, rather they are both
 // a naming convention and a usage convention so it's possible to create any of these yourself.
 // See the README at https://github.com/grafana/prometheus-arduino for more info.
-
 // Define a TimeSeries which can hold up to 5 samples, has a name of `uptime_milliseconds`
-TimeSeries ts1(5, (char*)"uptime_milliseconds_total", (char*)"{job=\"esp32-test\",host=\"richih1\"}");
-
-// Define a TimeSeries which can hold up to 5 samples, has a name of `heap_free_bytes`
-TimeSeries ts2(5, (char*)"heap_free_bytes", (char*)"{job=\"esp32-test\",host=\"richih1\",foo=\"bar\"}");
-
-//TimeSeries ts3(5, (char*)"kraken_temperature_celsius", (char*)"{job=\"esp32-test\",host=\"esp_1\",location=\"office\"}");
-//TimeSeries ts3(5, (char*)"foobar", (char*)"{job=\"esp32-test\",host=\"esp_1\",location=\"office\"}");
-TimeSeries ts3(5, (char*)"foobar", (char*)"{job=\"esp32-test\",host=\"richih1\",foo=\"bar\"}");
-
-TimeSeries ts4(5, (char*)"asdf", (char*)"{job=\"esp32-test\",host=\"richih1\",foo=\"bar\"}");
-
-
-// Note, metrics with the same name and different labels are actually different series and you would need to define them separately
-//TimeSeries ts2(5, "heap_free_bytes", "job=\"esp32-test\",host=\"richih1\",foo=\"bar\"");
+TimeSeries ts1(10, "uptime_milliseconds_total", "{job=\"esp32-test\",instance=\"esp01.office.hartmann.local\"}");
+TimeSeries ts2(10, "heap_free_bytes", "{job=\"esp32-test\",instance=\"esp01.office.hartmann.local\"}");
+TimeSeries ts3(10, "kraken_temperature_celsius", "{job=\"esp32-test\",instance=\"esp01.office.hartmann.local\"}");
+TimeSeries ts4(10, "kraken_pressure_pascal", "{job=\"esp32-test\",instance=\"esp01.office.hartmann.local\"}");
+TimeSeries ts5(10, "kraken_humidity_relative", "{job=\"esp32-test\",instance=\"esp01.office.hartmann.local\"}");
+TimeSeries ts6(10, "kraken_gas_resistance_ohm", "{job=\"esp32-test\",instance=\"esp01.office.hartmann.local\"}");
+TimeSeries ts7(10, "kraken_altitude_meter", "{job=\"esp32-test\",instance=\"esp01.office.hartmann.local\"}");
+TimeSeries ts8(10, "kraken_dew_point_celsius", "{job=\"esp32-test\",instance=\"esp01.office.hartmann.local\"}");
 
 int loopCounter = 0;
-
 void setup() {
     Serial.begin(115200);
-
     // Wait 5s for serial connection or continue without it
     // some boards like the esp32 will run whether or not the 
     // serial port is connected, others like the MKR boards will wait
@@ -58,11 +58,9 @@ void setup() {
         delay(100);
         serialTimeout++;
     }
-
     Serial.println("Starting");
     Serial.print("Free Mem Before Setup: ");
     Serial.println(freeMemory());
-
     // Configure and start the transport layer
     transport.setUseTls(true);
     transport.setCerts(grafanaCert, strlen(grafanaCert));
@@ -73,10 +71,8 @@ void setup() {
         Serial.println(transport.errmsg);
         while (true) {};
     }
-
     // Configure the client
     client.setUrl(GC_URL);
-    //client.setPath((char*)GC_PATH);
     client.setPath((char*)GC_PATH);
     client.setPort(GC_PORT);
     client.setUser(GC_USER);
@@ -86,21 +82,18 @@ void setup() {
         Serial.println(client.errmsg);
         while (true) {};
     }
-
-
     // Add our TimeSeries to the WriteRequest
     req.addTimeSeries(ts1);
     req.addTimeSeries(ts2);
     req.addTimeSeries(ts3);
     req.addTimeSeries(ts4);
-
+    req.addTimeSeries(ts5);
+    req.addTimeSeries(ts6);
+    req.addTimeSeries(ts7);
+    req.addTimeSeries(ts8);
     req.setDebug(Serial);  // Remove this line to disable debug logging of the write request serialization and compression.
-
     Serial.print("Free Mem After Setup: ");
     Serial.println(freeMemory());
-
-    Serial.println(F("--- --- --- ---"));
-    Serial.println(F("BME680 async test"));
 
     if (!bme.begin()) {
       Serial.println(F("Could not find a valid BME680 sensor, check wiring!"));
@@ -115,58 +108,28 @@ void setup() {
     bme.setGasHeater(320, 150); // 320*C for 150 ms
 
 };
-
-
-
 void loop() {
 
-    // Tell BME680 to begin measurement.
-    unsigned long endTime = bme.beginReading();
-    if (endTime == 0) {
-    Serial.println(F("Failed to begin reading :("));
-      return;
+    if (!bme.begin()) {
+      Serial.println(F("Could not find a valid BME680 sensor, check wiring!"));
+      while (1);
     }
-    Serial.print(F("Reading started at "));
-    Serial.print(millis());
-    Serial.print(F(" and will finish at "));
-    Serial.println(endTime);
 
-  if (!bme.endReading()) {
-    Serial.println(F("Failed to complete reading :("));
-    return;
-  }
-  Serial.print(F("Reading completed at "));
-  Serial.println(millis());
+    // Set up oversampling and filter initialization
+    bme.setTemperatureOversampling(BME680_OS_8X);
+    bme.setHumidityOversampling(BME680_OS_2X);
+    bme.setPressureOversampling(BME680_OS_4X);
+    bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+    bme.setGasHeater(320, 150); // 320*C for 150 ms
 
-  Serial.print(F("Temperature = "));
-  Serial.print(bme.temperature);
-  Serial.println(F(" *C"));
+    saturationVaporPressurehPa = 6.1078 * powf(10, ((a * bme.temperature) / (b + bme.temperature)));
+    vaporPressurehPa = (bme.humidity / 100) * saturationVaporPressurehPa;
+    v = log10(vaporPressurehPa/6.1078);
+    dewPointCelsius = b * v / (a - v);
 
-  Serial.print(F("Pressure = "));
-  Serial.print(bme.pressure / 100.0);
-  Serial.println(F(" hPa"));
-
-  Serial.print(F("Humidity = "));
-  Serial.print(bme.humidity);
-  Serial.println(F(" %"));
-
-  Serial.print(F("Gas = "));
-  Serial.print(bme.gas_resistance / 1000.0);
-  Serial.println(F(" KOhms"));
-
-  Serial.print(F("Approx. Altitude = "));
-  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial.println(F(" m"));
-
-  Serial.println();
-  
     int64_t time;
     time = transport.getTimeMillis();
     Serial.println(time);
-//
-
-
-
     // Efficiency in requests can be gained by batching writes so we accumulate 5 samples before sending.
     // This is not necessary however, especially if your writes are infrequent, but it's recommended to build batches when you can.
     if (loopCounter >= 5) {
@@ -186,27 +149,37 @@ void loop() {
         ts2.resetSamples();
         ts3.resetSamples();
         ts4.resetSamples();
-
+        ts5.resetSamples();
+        ts6.resetSamples();
+        ts7.resetSamples();
+        ts8.resetSamples();
     }
     else {
         if (!ts1.addSample(time, millis())) {
             Serial.println(ts1.errmsg);
         }
-        //if (!ts2.addSample(time, freeMemory())) {
-        if (!ts2.addSample(time, bme.temperature)) {
+        if (!ts2.addSample(time, freeMemory())) {
             Serial.println(ts2.errmsg);
         }
-        if (!ts3.addSample(time, freeMemory())) {
-      //  if (!ts3.addSample(time, (bme.temperature))) {
+        if (!ts3.addSample(time, bme.temperature)) {
             Serial.println(ts3.errmsg);
         }
-        if (!ts4.addSample(time, bme.temperature)) {
+        if (!ts4.addSample(time, bme.pressure)) {
             Serial.println(ts4.errmsg);
         }
+        if (!ts5.addSample(time, bme.humidity)) {
+            Serial.println(ts5.errmsg);
+        }
+        if (!ts6.addSample(time, bme.gas_resistance)) {
+            Serial.println(ts6.errmsg);
+        }
+        if (!ts7.addSample(time, bme.readAltitude(SEALEVELPRESSURE_HPA))) {
+            Serial.println(ts7.errmsg);
+        }
+        if (!ts8.addSample(time, dewPointCelsius)) {
+            Serial.println(ts8.errmsg);
+        }       
         loopCounter++;
     }
-
     delay(500);
-
-  //delay(5000);
 };
--- prom_02_grafana_cloud/prom_02_grafana_cloud.ino
+++ prom_02_grafana_cloud/prom_02_grafana_cloud.ino
@@ -2,33 +2,28 @@
 #include "certificates.h"
 #include <PromLokiTransport.h>
 #include <PrometheusArduino.h>
-
 PromLokiTransport transport;
 PromClient client(transport);
-
 // Create a write request for 2 series.
-WriteRequest req(2, 1024);
-
+WriteRequest req(10, 2048);
 // Check out https://prometheus.io/docs/practices/naming/ for metric naming and label conventions.
 // This library does not currently create different metric types like gauges, counters, and histograms
 // however internally Prometheus does not differentiate between these types, rather they are both
 // a naming convention and a usage convention so it's possible to create any of these yourself.
 // See the README at https://github.com/grafana/prometheus-arduino for more info.
-
 // Define a TimeSeries which can hold up to 5 samples, has a name of `uptime_milliseconds`
-TimeSeries ts1(5, (char*)"uptime_milliseconds_total", (char*)"{job=\"esp32-test\",host=\"richih1\"}");
-
+TimeSeries ts1(5, "uptime_milliseconds_total", "{job=\"esp32-test\",host=\"esp32\"}");
 // Define a TimeSeries which can hold up to 5 samples, has a name of `heap_free_bytes`
-TimeSeries ts2(5, (char*)"heap_free_bytes", (char*)"{job=\"esp32-test\",host=\"richih1\",foo=\"bar\"}");
-
+TimeSeries ts2(5, "heap_free_bytes", "{job=\"esp32-test\",host=\"esp32\",foo=\"bar\"}");
+TimeSeries ts3(5, "heap_free_bytes_10", "{job=\"esp32-test\",host=\"esp32\",foo=\"bar\"}");
+TimeSeries ts4(5, "heap_free_bytes_100", "{job=\"esp32-test\",host=\"esp32\",foo=\"bar\"}");
+TimeSeries ts5(5, "heap_free_bytes_1000", "{job=\"esp32-test\",host=\"esp32\",foo=\"bar\"}");
+TimeSeries ts6(5, "heap_free_bytes_10000", "{job=\"esp32-test\",host=\"esp32\",foo=\"bar\"}");
 // Note, metrics with the same name and different labels are actually different series and you would need to define them separately
-//TimeSeries ts2(5, "heap_free_bytes", "job=\"esp32-test\",host=\"richih1\",foo=\"bar\"");
-
+//TimeSeries ts2(5, "heap_free_bytes", "job=\"esp32-test\",host=\"esp32\",foo=\"bar\"");
 int loopCounter = 0;
-
 void setup() {
     Serial.begin(115200);
-
     // Wait 5s for serial connection or continue without it
     // some boards like the esp32 will run whether or not the 
     // serial port is connected, others like the MKR boards will wait
@@ -38,11 +33,9 @@ void setup() {
         delay(100);
         serialTimeout++;
     }
-
     Serial.println("Starting");
     Serial.print("Free Mem Before Setup: ");
     Serial.println(freeMemory());
-
     // Configure and start the transport layer
     transport.setUseTls(true);
     transport.setCerts(grafanaCert, strlen(grafanaCert));
@@ -53,10 +46,8 @@ void setup() {
         Serial.println(transport.errmsg);
         while (true) {};
     }
-
     // Configure the client
     client.setUrl(GC_URL);
-    //client.setPath((char*)GC_PATH);
     client.setPath((char*)GC_PATH);
     client.setPort(GC_PORT);
     client.setUser(GC_USER);
@@ -66,25 +57,21 @@ void setup() {
         Serial.println(client.errmsg);
         while (true) {};
     }
-
-
     // Add our TimeSeries to the WriteRequest
     req.addTimeSeries(ts1);
     req.addTimeSeries(ts2);
+    req.addTimeSeries(ts3);
+    req.addTimeSeries(ts4);
+    req.addTimeSeries(ts5);
+    req.addTimeSeries(ts6);
     req.setDebug(Serial);  // Remove this line to disable debug logging of the write request serialization and compression.
-
     Serial.print("Free Mem After Setup: ");
     Serial.println(freeMemory());
-
 };
-
-
-
 void loop() {
     int64_t time;
     time = transport.getTimeMillis();
     Serial.println(time);
-
     // Efficiency in requests can be gained by batching writes so we accumulate 5 samples before sending.
     // This is not necessary however, especially if your writes are infrequent, but it's recommended to build batches when you can.
     if (loopCounter >= 5) {
@@ -102,6 +89,10 @@ void loop() {
         // Reset batches after a succesful send.
         ts1.resetSamples();
         ts2.resetSamples();
+        ts3.resetSamples();
+        ts4.resetSamples();
+        ts5.resetSamples();
+        ts6.resetSamples();
     }
     else {
         if (!ts1.addSample(time, millis())) {
@@ -110,9 +101,19 @@ void loop() {
         if (!ts2.addSample(time, freeMemory())) {
             Serial.println(ts2.errmsg);
         }
+        if (!ts3.addSample(time, freeMemory()*10)) {
+            Serial.println(ts2.errmsg);
+        }
+        if (!ts4.addSample(time, freeMemory()*100)) {
+            Serial.println(ts2.errmsg);
+        }
+        if (!ts5.addSample(time, freeMemory()*1000)) {
+            Serial.println(ts2.errmsg);
+        }
+        if (!ts6.addSample(time, freeMemory()*10000)) {
+            Serial.println(ts2.errmsg);
+        }
         loopCounter++;
     }
-
     delay(500);
-
 };
