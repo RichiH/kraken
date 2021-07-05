90fabc767ee992a27e9b31e7bc61d0faea3753f0
Richard Hartmann <richih@richih.org>
2021-07-06 00:11:46 +0200

    Add default config
    Signed-off-by: Richard Hartmann <richih@richih.org>

---
+++ bme_680/config.h
@@ -0,0 +1,8 @@
+#define WIFI_SSID     "TODO"
+#define WIFI_PASSWORD "TODO"
+
+#define GC_URL "prometheus-blocks-prod-us-central1.grafana.net"
+#define GC_PATH "/api/prom/push"
+#define GC_PORT 443
+#define GC_USER "TODO"
+#define GC_PASS "TODO"
